73c387c62f98a4f66e13a29ee14c7bd6e1852850
silvalimapablo-jpg <silva.lima.pablo@escola.pr.gov.br>
2026-08-10 19:35:01 -0300

    ...

--- Script.js
+++ Script.js
@@ -1,9 +1,10 @@
+
 const botao = document.querySelector("button"); 
 botao.addEventListener("click", botaoclicado); 
 
 function botaoclicado() { 
     let texto = botao.querySelector("span"); 
+    
     let curtidasAtuais = parseInt(texto.textContent) || 0;
     texto.textContent = curtidasAtuais + 1; 
 }
-
